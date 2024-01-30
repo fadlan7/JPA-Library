b60198a07fc498dcd7189f5dcdf818ba211518f0
Fadlan Sayyidul Anam <inifadlan7@gmail.com>
2024-01-30 09:38:05 +0700

    [IMPROVE] call inventory service and menu (enigpusview) in Main class

--- src/com/enigma/enigpus_livecode/Main.java
+++ src/com/enigma/enigpus_livecode/Main.java
@@ -1,7 +1,13 @@
 package com.enigma.enigpus_livecode;
 
+import com.enigma.enigpus_livecode.service.InventoryService;
+import com.enigma.enigpus_livecode.service.InventoryServiceImpl;
+import com.enigma.enigpus_livecode.view.EnigpusView;
+
 public class Main {
     public static void main(String[] args) {
-
+        InventoryService inventoryService = new InventoryServiceImpl();
+        EnigpusView view = new EnigpusView(inventoryService);
+        view.run();
     }
 }
