b42d8ebbe2f32c292056471153ac69ac3e088fa3
Fadlan Sayyidul Anam <inifadlan7@gmail.com>
2024-01-30 09:26:10 +0700

    [FEATURE] add FileUtil for save and read object

---
+++ src/com/enigma/enigpus_livecode/utility/FileUtil.java
@@ -0,0 +1,39 @@
+package com.enigma.enigpus_livecode.utility;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+public class FileUtil {
+    private static final Path bookPath = Path.of("enigpus");
+
+    // menyimpan object
+    public static void saveObject(Object object) {
+        try (OutputStream os = Files.newOutputStream(bookPath)) {
+            try (ObjectOutputStream oos = new ObjectOutputStream(os)) {
+                oos.writeObject(object);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Object readObject() {
+        if (!Files.exists(bookPath)) {
+            saveObject(new ArrayList<>());
+        }
+
+        try (InputStream is = Files.newInputStream(bookPath)) {
+            try (ObjectInputStream oos = new ObjectInputStream(is)) {
+                return oos.readObject();
+            } catch (IOException | ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
